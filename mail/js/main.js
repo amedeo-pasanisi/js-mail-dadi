d525b1889b151ea138879ba2169e05f55b0b0f2a
amedeo <amedeopasanisi97@gmail.com>
2021-07-16 16:11:15 +0200

    correzione mail 1

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,23 +1,22 @@
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
-
 var lista = ["amedeo@gmail.com", "contessa@libero.it", "oflaga@outlook.com"];
 console.log("Lista email: " + lista);
 
+// Chiedi all’utente la sua email
 var email = prompt("inserisci la tua email");
 console.log("Email utente: " + email);
 
-var presente;
-
+// controlla che sia nella lista di chi può accedere
 for (i = 0; i < lista.length; i++) {
     
     if (lista[i] === email) {
-        email = presente
-        console.log("La tua email è nella lista");
+        var presente;
+        email = presente;
     }
 }
 
-if (email != presente) {
+// stampa un messaggio appropriato sull’esito del controllo.
+if (email = presente) {
+    console.log("La tua email è nella lista");
+} else {
     console.log("La tua email non è nella lista");
 }
